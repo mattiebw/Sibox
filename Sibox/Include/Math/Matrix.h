0a92fc545f1f13470ba55ae37c5cd3b5b8e3d948
Matt Ware <github@mattwa.re>
2025-02-28 17:06:28 +0000

    feat: Begin matrix class

--- Sibox/Include/Math/Matrix.h
+++ Sibox/Include/Math/Matrix.h
@@ -1,3 +1,18 @@
 ﻿#pragma once
 
-
+template<typename T>
+struct Matrix2x2
+{
+    // Create an identity matrix.
+    Matrix2x2()
+        : m_Data({{1, 0}, {0, 1}})
+    { }
+    
+    void Set(int row, int column, T value)
+    {
+        m_Data[row][column] = value;
+    }
+    
+private:
+    T m_Data[2][2];
+};
